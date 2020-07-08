f80ca75d2242bd28260175ea047c8f945083ec68
m-tln <tul.matvey@gmail.com>
2020-07-08 14:04:27 +0800

    I deleted comments

--- 77_round/A/main.cpp
+++ 77_round/A/main.cpp
@@ -10,29 +10,23 @@ int main()
         return 0;
     }
     s[s.length() - 1] == '1' ? s.append("0") : s.append("1");
-    //std::cout << c << std::endl;  
     for(int i = 1; i < s.length(); ++i)
     {
-        //std::cout << i << " ";
         if(s[i] == '1' && s[i] == s[i - 1])
         {
             ++c;
-            //std::cout << c << " ";
         }
         else
         {
             c = 1;
-            //std::cout << c << " ";
         }
         if (s[i] == '0' && s[i] == s[i - 1])
         {
             ++c2;
-            //std::cout << c2 << std::endl;
         }
         else
         {
             c2 = 1;
-            //std::cout << c2 << std::endl;
         }
         if(c >= 7 || c2 >= 7)
         {
